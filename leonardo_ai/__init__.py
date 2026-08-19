4cd82d46ee7eb1b4203ac4a0e95720cfcbc59f6b
Anurag Bimbisar Dongare <68319456+Anurag1@users.noreply.github.com>
2026-08-20 01:30:11 +0530

    feat: add Leonardo-inspired discovery engine

---
+++ leonardo_ai/__init__.py
@@ -0,0 +1,5 @@
+"""Leonardo-inspired knowledge-discovery loop for HONet."""
+
+from .engine import DiscoveryEngine, Observation, Hypothesis, ExperimentResult
+
+__all__ = ["DiscoveryEngine", "Observation", "Hypothesis", "ExperimentResult"]
